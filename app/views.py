9ef3418623088b860f6fe3ef2317dd3c99e62ae1
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2015-03-01 02:15:46 -0600

    views
    views

--- app/views.py
+++ app/views.py
@@ -1,24 +1,19 @@
 from app import app
 from flask import render_template, flash, redirect
 from .forms import RecipeInputForm
-import logic
+import classes
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	form = RecipeInputForm()
-	if form.validate_on_submit():
-		if form.recipe_1.data:
-			rID_1 = logic.get_rID(form.recipe_1.data)
-		if form.recipe_2.data:
-			rID_2 = logic.get_rID(form.recipe_2.data)
-		# if form.recipe_3:
-		# 	rID_3 = logic.get_rID(form.recipe_3)
-		# if form.recipe_4:
-		# 	rID_4 = logic.get_rID(form.recipe_4)
-		schedule = logic.Schedule(form.recipe_1.data, form.recipe_2.data)
-		# get directions
-		return render_template('vertical_timeline.html', schedule = schedule)
-	else:
-		flash('Sorry, there seems to be a problem with the form')
-	return render_template('index.html',
-		form=form)
+    form = RecipeInputForm()
+    if form.validate_on_submit():
+        if (form.recipe_1.data and form.recipe_2.data and form.ovens_numb.data and form.stoves_numb.data):
+            creator = classes.ScheduleCreator()
+            #timeline = creator.create_schedule(form.stoves_numb.data, form.ovens_numb.data, form.recipe_1.data, form.recipe_2.data) #this does nothing, timeline is none
+            creator.create_schedule(form.stoves_numb.data, form.ovens_numb.data, form.recipe_1.data, form.recipe_2.data) #this does nothing, timeline is none
+            return render_template('vertical_timeline.html', diction = Schedule(creator.S.export()))
+        else:
+            flash('Sorry, there seems to be a problem with the form')
+    return render_template('index.html',form=form)
+#    
+#@app.route('/vertical_timeline/
